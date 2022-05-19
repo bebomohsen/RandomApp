e40d0807aa29a0ece47fcf7c1e04a0497ce599f6
bebomohsen <69720736+bebomohsen@users.noreply.github.com>
2022-05-19 02:29:38 +0200

    configure the firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:random/firebase_options.dart';
 import 'package:random/src/root.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+DateTime.now().
   runApp(const Root());
 }
-
