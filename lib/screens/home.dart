d019b038575f15604121e45d6ef671d2cfd1d47b
bebomohsen <69720736+bebomohsen@users.noreply.github.com>
2022-05-22 16:53:19 +0200

    last changes

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -15,7 +15,8 @@ class _HomeState extends State<Home> {
         children: [
           ConstrainedBox(
             constraints: const BoxConstraints.expand(),
-            child: Image.asset(fit: BoxFit.cover, 'assets/images/background.jpg'),
+            child:
+                Image.asset(fit: BoxFit.cover, 'assets/images/background.jpg'),
           ),
           Scaffold(
             backgroundColor: Colors.transparent,
@@ -23,15 +24,46 @@ class _HomeState extends State<Home> {
               children: [
                 Padding(
                   padding: const EdgeInsets.all(15.0),
-                  child: FittedBox(
-                    child: Row(
-                      children: [
-                        Image.network('https://i.ibb.co/ct7FWJs/Pngtree-cartoon-language-icon-free-clipart-4628069.png',width: 50),
-                        Text(' Welcome, Ahmed!',style: TextStyle(fontFamily: 'Skranji',fontSize: 20),)
-                      ],
-                    ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Image.network(
+                              'https://i.ibb.co/ct7FWJs/Pngtree-cartoon-language-icon-free-clipart-4628069.png',
+                              width: 50),
+                          Text(
+                            ' Welcome Agian!',
+                            style:
+                                TextStyle(fontFamily: 'Skranji', fontSize: 20,color: Colors.purple),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        width: 30,
+                      ),
+                      Icon(
+                        Icons.door_front_door_outlined,
+                        color: Colors.red,
+                        size: 25,
+                      ),
+                    ],
                   ),
-                )
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.blueGrey),
+                    child: Center(
+                      child: Text(
+                        'Iam so sorry , but I didnot have the time to finish my project , I only did sign in with Email and password , sign up with email and password with email verification and also you can sign in anonymous by firebase\nThanks alot for all these valuable content and i wish all good for you\nAhmed AbdElmohsen Ali Diab',
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(color: Colors.white,
+                            fontWeight: FontWeight.bold, fontSize: 30),
+                      ),
+                    ))
               ],
             ),
           )
